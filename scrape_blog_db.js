b04c82c32c6c44768f3b1b1682bda489104ae60b
Sam Harrison <56735839+SamHarrison98@users.noreply.github.com>
2020-07-13 15:54:05 +0100

    Add files via upload

---
+++ scrape_blog_db.js
@@ -0,0 +1,19 @@
+
+  var txt = ""
+  fetch("https://potion-api.now.sh/table?id=be173b7ca74c48ca80d7a8b2dbd3f9dc")
+  .then(res => res.json())
+  .then(json => {
+    for (element of json) {
+      txt += "<div class=\"container\" style=\"padding-bottom: 20px; padding-top: 20px\">\
+      <a href=\"blog-template.html?id=" + element.id + "&title=" + element.fields.Title + "\">\
+      <div id=" + element.id + " class=\"button is-fullwidth is-large is-primary is-bold\" style=\"height: 100px;\">\
+      <article class=\"media\">\
+      <figure class=\"media-content\">\
+      <h1 class=\"title is-size-4\">" + element.fields.Title + "</h1>" +
+      "<h2 class=\"subtitle\">" + element.fields.Date.start_date + "</h2>" +
+      "</figure></article></div></a></div>"
+      document.getElementById("Blog_Posts").innerHTML = txt;
+      console.log(json)
+
+  }
+})
---
+++ script.js
@@ -0,0 +1,14 @@
+
+var notionDocId = parent.document.URL.substring(parent.document.URL.indexOf('?'), parent.document.URL.length);
+notionDocId = notionDocId.substring(4);
+console.log(notionDocId);
+var title = parent.document.URL.substring(parent.document.URL.indexOf('title'), parent.document.URL.length);
+title = title.substring(6);
+title = title.replace(/%20/g, " ");
+
+fetch("https://potion-api.now.sh/html?id=" + notionDocId)
+  .then(res => res.text())
+  .then(text => {
+    console.log(text)
+    document.querySelector("main").innerHTML = "<h1 class=\"title\" style=\"text-align: center; padding-top: 40px;\">" + title + "</h1>" + text
+  })
